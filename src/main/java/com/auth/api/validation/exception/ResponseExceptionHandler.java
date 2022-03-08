9e81b3e727f982f9507230978f5a5497b17a8953
luisedu.unifal <luisedu.unifal@gmail.com>
2022-03-08 08:34:41 -0300

    Ultimo ajuste nas validacoes

--- src/main/java/com/auth/api/validation/exception/ResponseExceptionHandler.java
+++ src/main/java/com/auth/api/validation/exception/ResponseExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.auth.api.validation.exception;
 
 import com.auth.api.dto.response.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,16 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
         Response response =
                 new Response.MessageResponse(ex.getMessage(),
                              HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Response response =
+                new Response.MessageResponse(ex.getMessage(),
+                        HttpStatus.BAD_REQUEST.value());
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
